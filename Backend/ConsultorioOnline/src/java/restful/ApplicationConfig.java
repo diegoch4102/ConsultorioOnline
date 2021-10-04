98c6d6d421560d7d9d44cf530cf17d5576df0208
Diego Ch <diegoch4102@gmail.com>
2021-10-04 13:56:51 -0500

    Borrado de override in ApplicationConfig; CorsFilter modificado con Pierre. Folder Frontend añadido

--- Backend/ConsultorioOnline/src/java/restful/ApplicationConfig.java
+++ Backend/ConsultorioOnline/src/java/restful/ApplicationConfig.java
@@ -15,22 +15,5 @@ import javax.ws.rs.core.Application;
 @javax.ws.rs.ApplicationPath("app")
 public class ApplicationConfig extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(restful.GenericResource.class);
-        resources.add(restful.resource.PacienteResource.class);
-    }
     
 }
--- Backend/ConsultorioOnline/src/java/restful/Model/CorsFilter.java
+++ Backend/ConsultorioOnline/src/java/restful/Model/CorsFilter.java
@@ -20,7 +20,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-
+/**
+ *
+ * @author SENA
+ */
 @WebFilter(filterName = "corsFilter", urlPatterns = "/*",
         initParams = {@WebInitParam(name = "allowOrigin", value = "*"),
                 @WebInitParam(name = "allowMethods", value = "GET,POST,PUT,DELETE,OPTIONS"),
@@ -47,35 +50,48 @@ public class CorsFilter implements Filter{
     @Override
     public void doFilter(
             ServletRequest servletRequest,
-            ServletResponse servletResponse,
-            FilterChain filterChain) throws IOException, ServletException {
+            ServletResponse servletResponse, 
+            FilterChain filterChain) 
+            throws IOException, ServletException {
+        
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        if (!StringUtils.isEmpty(allowOrigin)) {
-            if(allowOrigin.equals("*")){
-                response.setHeader("Access-Control-Allow-Origin", allowOrigin);
-            }else{
-                List<String> allowOriginList = Arrays.asList(allowOrigin.split(","));
-                if (allowOriginList != null && allowOriginList.size() > 0) {
-                    String currentOrigin = request.getHeader("Origin");
-                    if (allowOriginList.contains(currentOrigin)) {
-                        response.setHeader("Access-Control-Allow-Origin", currentOrigin);
-                    }
-                }
-            }
-        }
-        if (!StringUtils.isEmpty(allowMethods)) {
-            response.setHeader("Access-Control-Allow-Methods", allowMethods);
-        }
-        if (!StringUtils.isEmpty(allowCredentials)) {
-            response.setHeader("Access-Control-Allow-Credentials", allowCredentials);
-        }
-        if (!StringUtils.isEmpty(allowHeaders)) {
-            response.setHeader("Access-Control-Allow-Headers", allowHeaders);
-        }
-        if (!StringUtils.isEmpty(exposeHeaders)) {
-            response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
-        }
+        
+//        if (!StringUtils.isEmpty(allowOrigin)) {
+//            if(allowOrigin.equals("*")){
+//                response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+//            }else{
+//                List<String> allowOriginList = Arrays.asList(allowOrigin.split(","));
+//                if (allowOriginList != null && allowOriginList.size() > 0) {
+//                    String currentOrigin = request.getHeader("Origin");
+//                    if (allowOriginList.contains(currentOrigin)) {
+//                        response.setHeader("Access-Control-Allow-Origin", currentOrigin);
+//                    }
+//                }
+//            }
+//        }
+//        if (!StringUtils.isEmpty(allowMethods)) {
+//            response.setHeader("Access-Control-Allow-Methods", allowMethods);
+//        }
+//        if (!StringUtils.isEmpty(allowCredentials)) {
+//            response.setHeader("Access-Control-Allow-Credentials", allowCredentials);
+//        }
+//        if (!StringUtils.isEmpty(allowHeaders)) {
+//            response.setHeader("Access-Control-Allow-Headers", allowHeaders);
+//        }
+//        if (!StringUtils.isEmpty(exposeHeaders)) {
+//            response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
+//        }
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
+        
+        response.setStatus(HttpServletResponse.SC_OK);
+         
+        filterChain.doFilter(servletRequest, response);
+        
         filterChain.doFilter(servletRequest, servletResponse);
     }
  
